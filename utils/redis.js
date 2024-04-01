16447d4a51a6b1c354f4ad4fcc66f59241aaa6d5
aziinova <aziazinnova@gmail.com>
2024-04-01 18:59:19 +0300

    a

---
+++ utils/redis.js
@@ -0,0 +1,67 @@
+// utils/redis.js
+
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    // Display any errors in the console
+    this.client.on('error', (err) => {
+      console.error('Redis error:', err);
+    });
+  }
+
+  async isAlive() {
+    return new Promise((resolve) => {
+      this.client.ping((err, result) => {
+        if (err) {
+          resolve(false);
+        } else {
+          resolve(result === 'PONG');
+        }
+      });
+    });
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
+
